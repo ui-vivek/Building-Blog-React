3e3c3c3882c88d07aea3a13106bd1b444c2ec59e
Vivek Singh <cu.vivek.1557@gmail.com>
2023-03-02 19:07:26 +0530

    removing unwanted texts

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -1,29 +1,11 @@
-import React from "react";
-import Header from "./NavBar/Header";
+import React from 'react'
+import Header from './NavBar/Header'
 
 export default function About() {
   return (
-    <div style={{ backgroundColor: "lightpink" }}>
-      <Header />
-      <h3>About Page</h3>
-      <p>
-        Welcome to our About page! We are a team of passionate individuals who
-        are dedicated to providing quality services and products to our
-        customers. Our mission is to make a positive impact in our community by
-        offering innovative solutions and exceptional customer support. At our
-        core, we value integrity, honesty, and transparency. We believe in
-        fostering strong relationships with our customers and partners, and we
-        are committed to delivering on our promises. We continuously strive to
-        improve our offerings and processes to ensure that we are providing the
-        best possible experience for everyone who interacts with us. We are
-        proud of our achievements and the impact that we have made so far.
-        However, we are constantly looking for ways to grow and evolve to meet
-        the changing needs of our customers and the world. We welcome feedback
-        and suggestions from our community to help us improve and reach our
-        goals. Thank you for taking the time to learn more about us. If you have
-        any questions or would like to know more, please don't hesitate to
-        contact us.
-      </p>
-    </div>
-  );
+    <>
+    <Header/>
+    <p>About Page</p>
+    </>
+  )
 }
--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -1,29 +1,12 @@
 import Header from "./NavBar/Header";
 
-const Home = () => {
-  return (
-    <div style={{ backgroundColor: "lightgreen" }}>
-      <Header />
-      <h3>Home Page</h3>
-      <p>
-        Welcome to our website! We are a team of experts dedicated to providing
-        high-quality services to our clients. Our mission is to make your life
-        easier and more convenient by providing a range of services to help you
-        achieve your goals. Whether you need assistance with financial planning,
-        investment management, or other financial services, we are here to help.
-        Our team is comprised of highly trained professionals who are committed
-        to delivering personalized solutions that meet your unique needs. We
-        understand that every client is different, which is why we take the time
-        to get to know you and your specific goals. We believe that by working
-        closely with you, we can create a customized plan that will help you
-        achieve your financial objectives and live the life you want. Thank you
-        for visiting our website. We hope you find the information and services
-        you are looking for. If you have any questions or would like to learn
-        more about how we can help you, please don't hesitate to contact us. We
-        look forward to working with you!
-      </p>
-    </div>
-  );
-};
+const Home=()=>{
+   return (
+    <>
+    <Header/>
+    <p>Home Page</p>
+    </>
+   )
+}
 
 export default Home;
