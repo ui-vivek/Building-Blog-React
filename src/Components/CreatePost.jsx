336d4373d2f11cd58229e6289a8410843c065c71
Vivek singh <cu.vivek.1557@gmail.com>
2023-03-03 11:25:19 +0530

    shift all create-post for home to CreatePost page

--- src/Components/CreatePost.jsx
+++ src/Components/CreatePost.jsx
@@ -1,8 +1,25 @@
-import React from "react";
-import { useNavigate } from "react-router-dom";
+import React , {useState} from "react";
+import {  useNavigate } from "react-router-dom";
 import Header from "./NavBar/Header";
+import handleSubmit from '../Firebase/handlesubmit'
 
 export default function About() {
+  const [title, setTitle] = useState();
+  const [subtitle, setSubitle] = useState();
+  const [post, setPost] = useState();
+
+  const printData = (e) => {
+    e.preventDefault(); // this is used to prevent form reloding the page
+    console.log("Title", title);
+    console.log("SubTitle", subtitle);
+    console.log("Post", post);
+    handleSubmit({
+      title,
+      subtitle,
+      post,
+      createAt : new Date()
+    })
+  };
   const navigate = useNavigate();
   const goto = () => {
     navigate("/");
@@ -12,6 +29,35 @@ export default function About() {
       <Header />
       <p>Create A Post</p>
       <button onClick={goto}>Go to Home</button>
+      <form onSubmit={printData} className="container">
+        <div className="form-field">
+          <label>Title</label>
+          <input
+            type="text"
+            onChange={(e) => {
+              setTitle(e.target.value);
+            }}
+          />
+        </div>
+        <div className="form-field">
+          <label>Sub Title</label>
+          <input
+            type="text"
+            onChange={(e) => {
+              setSubitle(e.target.value);
+            }}
+          />
+        </div>
+        <div className="form-field">
+          <label>Post</label>
+          <textarea
+            onChange={(e) => {
+              setPost(e.target.value);
+            }}
+          ></textarea>
+        </div>
+        <button className="create-post-btn">Create Post</button>
+      </form>
     </>
   );
 }
--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -1,27 +1,11 @@
-import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import Header from "./NavBar/Header";
-import handleSubmit from '../Firebase/handlesubmit'
+
 
 const Home = () => {
-  const [title, setTitle] = useState();
-  const [subtitle, setSubitle] = useState();
-  const [post, setPost] = useState();
   const navigate = useNavigate(); // useNavigater hook is use to nevigate to another page
   const goToCreatePost = () => {
     navigate("/createPost"); // we can pass the value as (-1) and it will redirect back to the pre page
-  };
-  const printData = (e) => {
-    e.preventDefault(); // this is used to prevent form reloding the page
-    console.log("Title", title);
-    console.log("SubTitle", subtitle);
-    console.log("Post", post);
-    handleSubmit({
-      title,
-      subtitle,
-      post,
-      createAt : new Date()
-    })
   };
   return (
     <>
@@ -29,35 +13,7 @@ const Home = () => {
       <p>Home Page</p>
       <button onClick={goToCreatePost}>Go to Create Post</button>
 
-      <form onSubmit={printData} className="container">
-        <div className="form-field">
-          <label>Title</label>
-          <input
-            type="text"
-            onChange={(e) => {
-              setTitle(e.target.value);
-            }}
-          />
-        </div>
-        <div className="form-field">
-          <label>Sub Title</label>
-          <input
-            type="text"
-            onChange={(e) => {
-              setSubitle(e.target.value);
-            }}
-          />
-        </div>
-        <div className="form-field">
-          <label>Post</label>
-          <textarea
-            onChange={(e) => {
-              setPost(e.target.value);
-            }}
-          ></textarea>
-        </div>
-        <button className="create-post-btn">Create Post</button>
-      </form>
+      
     </>
   );
 };
