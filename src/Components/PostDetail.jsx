e855acbc58781a4ed6078d689f1930b8be246483
vivek singh <cu.vivek.1557@gmail.com>
2023-03-04 12:06:01 +0530

    Display Post Details on Page

--- src/Components/PostDetail.jsx
+++ src/Components/PostDetail.jsx
@@ -1,7 +1,30 @@
-import React from 'react'
+import React, { useState,useEffect } from "react";
+import { useParams } from "react-router-dom";
+import { firestore } from "../Firebase/firebase";
+import Header from "./NavBar/Header";
 
 export default function PostDetail() {
+  const [post, setPost] = useState({});
+  const { postId } = useParams();
+
+  useEffect(() => {
+    firestore
+      .collection('posts')
+      .doc(postId)
+      .get()
+      .then((snapshot) => {
+        console.log("snapshot",snapshot.data());
+        setPost(snapshot.data())
+      });
+  }, [postId]);
+
   return (
-    <div>PostDetail</div>
+    <>
+    <Header/>
+    <div>
+      <h1>{post.title}</h1>
+      <p>{post.post}</p>
+    </div>
+    </>
   )
 }
